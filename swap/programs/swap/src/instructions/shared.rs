78972cb192f8dfbc92e6b6cdb6441e89a134f87e
Pratikkale26 <pratikkale7661@gmail.com>
2025-08-17 08:21:24 +0000

    make offer instructions done

--- swap/programs/swap/src/instructions/shared.rs
+++ swap/programs/swap/src/instructions/shared.rs
@@ -1,9 +1,23 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::TokenAccount;
+use anchor_spl::{token::{transfer_checked, TransferChecked}, token_interface::{TokenInterface, Mint, TokenAccount}};
 
 
 pub fn transfer_tokens<'info> (
-    from: &InterfaceAccount<'info, TokenAccount>
+    from: &InterfaceAccount<'info, TokenAccount>,
+    to: &InterfaceAccount<'info, TokenAccount>,
+    amount: &u64,
+    mint: &InterfaceAccount<'info, Mint>,
+    authority: &Signer<'info>,
+    token_program: &Interface<'info, TokenInterface>
 ) -> Result<()> {
+    let transfer_acc_options = TransferChecked {
+        from: from.to_account_info(),
+        mint: mint.to_account_info(),
+        to: to.to_account_info(),
+        authority: authority.to_account_info(),
+    };
 
+    let cpi_ctx = CpiContext::new(token_program.to_account_info(), transfer_acc_options);
+
+    transfer_checked(cpi_ctx, *amount, mint.decimals)
 }
--- swap/programs/swap/src/lib.rs
+++ swap/programs/swap/src/lib.rs
@@ -15,7 +15,9 @@ declare_id!("7xF1VC58R4954szvzDzvPypdjc7ox2dY7SWjtxcpxFy1");
 pub mod swap {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(ctx: Context<MakeOffer>, id: u64, token_a_offered_amount: u64, token_b_wanted_amount: u64) -> Result<()> {
+        instructions::make_offer::send_offered_token_to_vault(&ctx, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(ctx, id, token_b_wanted_amount)
+        
     }
 }
